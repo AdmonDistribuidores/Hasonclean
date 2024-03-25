a7e89225e847cce65989734cd8371fca567b690e
AdmonDistribuidores <96606876+AdmonDistribuidores@users.noreply.github.com>
2024-03-25 17:39:37 -0500

    Update main.js
    admoncaducado

--- js/main.js
+++ js/main.js
@@ -684,3 +684,27 @@ function agp31() {
     msj2();
      
 }
+
+//suscripcion>
+  function Caducada(){
+    let url = `https://cliconline.github.io/CliconlineStudios/`;
+    window.open(url); 
+   }
+
+
+          document.addEventListener('DOMContentLoaded', sus);
+
+    function sus (){
+        Swal.fire({
+            position: 'bottom',
+            icon: 'warning',
+            title: 'Suscripción Caducada',
+            text: 'Activa tu Tarjeta Digital',
+            allowOutsideClick: false,
+          footer: 'CliconlineStudios'
+        }).then((result) => {
+            if (result.isConfirmed) {
+              Caducada();
+            }
+          })
+    };
